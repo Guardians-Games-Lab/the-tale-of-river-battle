30cd147aa4dc5773166fc1746748de5db8f00762
AlissonCaldeira <alissoncaldeira.d@gmail.com>
2026-03-17 15:57:31 -0300

    Menu atualizado

--- Codes/scene_handler.gd
+++ Codes/scene_handler.gd
@@ -2,14 +2,21 @@ extends Node
 
 # Muda cena do jogo
 func _ready():
-	get_node("MainMenu/M/VB/NovoJogo").pressed.connect(on_new_game_pressed)
-	get_node("MainMenu/M/VB/Sair").pressed.connect(on_exit_pressed)
-
+	get_node("MainMenu/M/MenuOptions/NovoJogo").pressed.connect(on_new_game_pressed)
+	get_node("MainMenu/M/MenuOptions/Sair").pressed.connect(on_exit_pressed)
+	get_node("MainMenu/M/MatchMaking/LanMode").pressed.connect(on_lanmode_pressed)
 
 func on_new_game_pressed():
 	get_node("MainMenu").queue_free();
-	var game_scene = load("res://Assets/map_1.tscn").instantiate()
+	var game_scene = load("res://Scenes/MainScenes/game_scene.tscn").instantiate()
 	add_child(game_scene)
 	
 func on_exit_pressed():
 	get_tree().quit()
+func on_lanmode_pressed():
+	var label = $MainMenu/M/MatchMaking/LanMode/Label
+	if label.text == "Offline":
+		label.text = "Online"
+	else:
+		label.text = "Offline"
+	
